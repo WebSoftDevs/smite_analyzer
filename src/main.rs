da613b30d9dbbf829ff49b326c3f9374fd6bac10
Mateusz <morphe@MacBook-Air-Mateusz.local>
2023-03-06 21:30:06 +0100

    Added clippy::pedantic

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
+#![warn(clippy::pedantic)]
 use dotenv::dotenv;
 
-
 fn main() {
-    dotenv().ok();
+      dotenv().ok();
     let smite_api_key = std::env::var("SMITE_API_KEY").expect("SMITE_API_KEY not found.");
     if smite_api_key.chars().count() == 0 {
         panic!("Value for key SMITE_API_KEY is empty.");
     }
-    
 }
